4a508ca32567343ede50d07d6dc65003b08f8b61
Tirth24052002 <tirth.p@simformsolutions.com>
2023-02-23 16:58:40 +0530

    commit-four

---
+++ swiftexercise.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -0,0 +1,5 @@
+import UIKit
+//Write a Swift program to create a new string made of a copy of the first two characters of a given string. If the given string is shorter than length 2, return whatever there is.
+var String1 = "Simform"
+print(String1.prefix(2))
+
