e9d5848d10a74c41ed3206e6aede97d1d5e71163
Tirth24052002 <tirth.p@simformsolutions.com>
2023-02-24 14:28:00 +0530

    -Swift- string- exercise

--- swiftexercise.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
+++ swiftexercise.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -1,2 +1,64 @@
 import UIKit
 
+var greeting = "Hello, playground"
+//print variables of different types and print it.
+let b = 10
+var a = 5
+print("a")
+print("b")
+a += 2
+
+let d = 10
+var c = 5
+
+let (e,f) = (1,2)
+
+//Arithmetic Operators
+1+2
+33-11
+2*3
+10/2
+
+"hello, " + "world"
+
+9%4//Reminder Operator
+-9%4
+
+//Unary minus and plus operator
+let three = 3
+let minusThree = -three
+let plusThree = -minusThree
+
+let minusSix = -6
+let alsoMinusSix = +minusSix
+
+var x = 1
+x += 2
+
+//Comparison Operators
+1 == 1
+2 != 1
+2 > 1
+1 < 2
+1 >= 1
+2 <= 1
+
+let name = "Worlds"
+if name == "World" {
+    print("hello, world")
+}else {
+    print("I'm sorry \(name), but I don't recognize you")
+}
+
+(1, "zebra") < (2, "apple")
+(3, "apple") < (3, "bird")
+(4, "dog") == (4, "dog")
+
+
+let contentHeight  = 40
+let hasHeader  = true
+let rowHeight = contentHeight + (hasHeader ? 50 : 20)
+
+//----------------------
+var age = 5
+var marks, sum : Int
